d8197c138c04802fa65464031da2557b9a3e7a88
Yves Brissaud <yves@sogilis.com>
2015-10-13 21:58:30 +0200

    #1: Read changelog from git repository

--- spec/valr_spec.rb
+++ spec/valr_spec.rb
@@ -2,15 +2,13 @@ require 'valr'
 
 describe Valr do
   describe '#changelog' do
-    let(:commits) {
-      @commits = ["first commit\n\nplop",
-                  "2nd commit\n\nplop",
-                  "3rd commit\n\nplop"]
+    let(:repo_path) {
+      @repo_path = 'fixtures/simple_repo.git'
     }
     context 'without any specific formating' do
       it 'returns the first line of commit messages in markdown list' do
         valr = Valr.new
-        expect(valr.changelog(commits)).to eq "- 3rd commit\n- 2nd commit\n- first commit"
+        expect(valr.changelog(repo_path)).to eq "- 3rd commit\n- 2nd commit\n- first commit"
       end
     end
   end
