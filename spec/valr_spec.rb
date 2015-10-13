e8e9d38f354bf36571e11edc158176c55281e15b
Yves Brissaud <yves@sogilis.com>
2015-10-13 21:54:02 +0200

    #1: Refactor test

--- spec/valr_spec.rb
+++ spec/valr_spec.rb
@@ -1,15 +1,17 @@
 require 'valr'
 
-RSpec.describe Valr, '#changelog' do
-  let(:commits) {
-    @commits = ["first commit\n\nplop",
-                "2nd commit\n\nplop",
-                "3rd commit\n\nplop"]
-  }
-  context 'without any specific formating' do
-    it 'returns the first line of commit messages in markdown list' do
-      valr = Valr.new
-      expect(valr.changelog(commits)).to eq "- 3rd commit\n- 2nd commit\n- first commit"
+describe Valr do
+  describe '#changelog' do
+    let(:commits) {
+      @commits = ["first commit\n\nplop",
+                  "2nd commit\n\nplop",
+                  "3rd commit\n\nplop"]
+    }
+    context 'without any specific formating' do
+      it 'returns the first line of commit messages in markdown list' do
+        valr = Valr.new
+        expect(valr.changelog(commits)).to eq "- 3rd commit\n- 2nd commit\n- first commit"
+      end
     end
   end
 end
