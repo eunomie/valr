69ac5020be82a4a97c9e030197821cad45c90de5
Yves Brissaud <yves@sogilis.com>
2015-10-13 23:31:11 +0200

    #1: Create git fixtures on the fly

--- spec/valr_spec.rb
+++ spec/valr_spec.rb
@@ -1,10 +1,15 @@
 require 'valr'
 
 describe Valr do
+  before(:each) do
+    create_simple_fixtures
+  end
+
+  after(:each) do
+    clear_fixtures
+  end
+
   describe '#changelog' do
-    let(:repo_path) {
-      @repo_path = File.join File.dirname(__FILE__), '..', 'fixtures/simple_repo.git'
-    }
     context 'without any specific formating' do
       it 'returns the first line of commit messages in markdown list' do
         valr = Valr.new
