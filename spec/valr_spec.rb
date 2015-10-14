62100e3df89c6621c09a1e2dc414918198bb862b
Yves Brissaud <yves.brissaud@gmail.com>
2015-10-14 23:58:40 +0200

    [#7] Rename to Valr::Repo

--- spec/valr_spec.rb
+++ spec/valr_spec.rb
@@ -12,7 +12,7 @@ describe Valr do
       end
 
       it 'raise an error' do
-        expect{Valr.new repo_path}.to raise_error
+        expect{Valr::Repo.new repo_path}.to raise_error
       end
     end
   end
@@ -24,7 +24,7 @@ describe Valr do
 
     context 'without any specific formating' do
       it 'returns the first line of commit messages in markdown list' do
-        valr = Valr.new repo_path
+        valr = Valr::Repo.new repo_path
         expect(valr.changelog).to eq "- 3rd commit\n- 2nd commit\n- first commit"
       end
     end
@@ -37,12 +37,12 @@ describe Valr do
 
     context 'without any specific formating' do
       it 'returns the sha1 of the commit as a context of the changelog' do
-        valr = Valr.new repo_path
+        valr = Valr::Repo.new repo_path
         expect(valr.full_changelog.lines.first.chomp).to match /^[0-9a-f]{40}$/
       end
 
       it 'returns a blank line and the commits in a markdown list after the metadata' do
-        valr = Valr.new repo_path
+        valr = Valr::Repo.new repo_path
         expect(valr.full_changelog.lines[1..-1].join).to eq "\n- 3rd commit\n- 2nd commit\n- first commit"
       end
     end
