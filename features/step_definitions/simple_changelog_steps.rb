aba2f35cb5ce95089267fd386e36bc19fcd73279
Yves Brissaud <yves.brissaud@gmail.com>
2016-01-29 20:35:07 +0100

    [#17] Add first and simple cucumber test

---
+++ features/step_definitions/simple_changelog_steps.rb
@@ -0,0 +1,11 @@
+Given(/^a repository$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^ask for the changelog$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^returns all commit messages$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
